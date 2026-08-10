579afc31013bda7db8d5c690504c447620bcaafd
Surender Dalal <surendermca04@gmail.com>
2026-08-10 13:59:23 -0400

    updated Spring Batch code

--- AutorunSpringBatch/src/main/java/com/jgsurender/springboot/batch/SpringBatchConceptsApplication.java
+++ AutorunSpringBatch/src/main/java/com/jgsurender/springboot/batch/SpringBatchConceptsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBatchConceptsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchConceptsApplication.class, args);
+		SpringApplication.run(SpringBatchConceptsApplication.class, args); 
 	}
 
 }
